50c0b0832db7b51cc2d713e20c3172e1ac0df487
TI_Eugene <ti.eugene@gmail.com>
2022-08-22 12:37:05 +0300

    CLI ok

--- iosc/main.py
+++ iosc/main.py
@@ -9,7 +9,7 @@ from mainwindow import MainWindow
 
 def main():
     # QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
-    app = QApplication()
+    app = QApplication(sys.argv)
     mw: QMainWindow = MainWindow()
     available_geometry = app.desktop().availableGeometry(mw)  # 0, 0, 1280, 768 (display height - taskbar)
     mw.resize(available_geometry.width() * 3 / 4, available_geometry.height() * 3 / 4)
