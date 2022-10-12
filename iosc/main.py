70512e6d367c83a4d70c7923e01658c2b53b26ad
TI_Eugene <ti.eugene@gmail.com>
2022-10-12 16:56:59 +0300

    bash compatible

--- iosc/main.py
+++ iosc/main.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python3
-# 1. std
+import os.path
 import sys
-# 2. 3rd
-from PyQt5.QtWidgets import QApplication, QMainWindow
-# 3. local
-from mainwindow import MainWindow
-
-
-def main() -> int:
-    # QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
-    app = QApplication(sys.argv)
-    mw: QMainWindow = MainWindow(sys.argv)
-    available_geometry = app.desktop().availableGeometry(mw)  # 0, 0, 1280, 768 (display height - taskbar)
-    mw.resize(int(available_geometry.width() * 3 / 4), int(available_geometry.height() * 3 / 4))
-    mw.show()
-    mw.handle_cli()
-    return app.exec_()
-
 
 if __name__ == "__main__":
+    if 'PYTHONPATH' not in os.environ:
+        sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+    from mainwindow import main
     sys.exit(main())
