0cd6b31b1bc904ffae489087be235911fcc97c83
TI_Eugene <ti.eugene@gmail.com>
2022-10-13 18:46:17 +0300

    Top's TmpPtr add/move ok

--- iosc/sig/widget/top.py
+++ iosc/sig/widget/top.py
@@ -7,43 +7,72 @@ from iosc.core import mycomtrade
 from iosc.sig.widget.common import CleanScrollArea
 
 
-class PtrLabelMain(QCPItemText):
-    __root: QWidget
+class PtrLabel(QCPItemText):
+    _root: QWidget
 
     def __init__(self, parent: QCustomPlot, root: QWidget):
         super().__init__(parent)
-        self.__root = root
+        self._root = root
         self.setTextAlignment(Qt.AlignCenter)
         self.setPadding(QMargins(2, 2, 2, 2))
         self.setPositionAlignment(Qt.AlignHCenter)  # | Qt.AlignTop (default)
         self.setFont(iosc.const.FONT_X)
         self.setColor(iosc.const.COLOR_LABEL_X)  # text
-        self.setBrush(iosc.const.BRUSH_PTR_MAIN)  # rect
-        self.__root.signal_ptr_moved_main.connect(self.slot_ptr_move)
 
-    def slot_ptr_move(self, i: int):
+
+class PtrLabelMain(PtrLabel):
+    def __init__(self, parent: QCustomPlot, root: QWidget):
+        super().__init__(parent, root)
+        self.setBrush(iosc.const.BRUSH_PTR_MAIN)  # rect
+        self._root.signal_ptr_moved_main.connect(self.__slot_ptr_move)
+
+    def __slot_ptr_move(self, i: int):
         """Repaint/move main ptr value label (%.2f)
         :fixme: draw in front of ticks
         """
-        x = self.__root.i2x(i)
+        x = self._root.i2x(i)
         self.setText("%.2f" % x)
         self.position.setCoords(x, 0)
         self.parent().replot()
 
 
+class PtrLabelTmp(PtrLabel):
+    _uid: int
+
+    def __init__(self, parent: QCustomPlot, root: QWidget, uid: int):
+        super().__init__(parent, root)
+        self._uid = uid
+        self.setBrush(iosc.const.BRUSH_PTR_TMP)  # rect
+        self._root.signal_ptr_moved_tmp.connect(self.__slot_ptr_move)
+
+    def __slot_ptr_move(self, uid: int, i: int):
+        """Repaint/move main ptr value label (%.2f)
+        :fixme: draw in front of ticks
+        """
+        if uid == self._uid:
+            x = self._root.i2x(i)
+            self.setText("%.2f" % x)
+            self.position.setCoords(x, 0)
+            self.parent().replot()
+
+
 class TimeAxisWidget(QCustomPlot):
     __root: QWidget
     __main_ptr_label: PtrLabelMain
+    _tmp_ptr: dict[int, PtrLabelTmp]
 
     def __init__(self, osc: mycomtrade.MyComtrade, root: QWidget, parent: CleanScrollArea):
         super().__init__(parent)
         self.__root = root
         self.__main_ptr_label = PtrLabelMain(self, root)
+        self._tmp_ptr = dict()
         t0 = osc.raw.trigger_time
         self.xAxis.setRange((osc.raw.time[0] - t0) * 1000, (osc.raw.time[-1] - t0) * 1000)
         self.__squeeze()
         self.__set_style()
         self.__root.signal_xscale.connect(self._slot_chg_width)
+        self.__root.signal_ptr_add_tmp.connect(self._slot_ptr_add_tmp)
+        self.__root.signal_ptr_del_tmp.connect(self._slot_ptr_del_tmp)
 
     def __squeeze(self):
         ar = self.axisRect(0)
@@ -66,3 +95,11 @@ class TimeAxisWidget(QCustomPlot):
         self.setFixedWidth(w_new)
         self.xAxis.ticker().setTickCount(iosc.const.TICK_COUNT * self.__root.xzoom)
         self.replot()
+
+    def _slot_ptr_add_tmp(self, uid: int):
+        """Add new TmpPtr"""
+        self._tmp_ptr[uid] = PtrLabelTmp(self, self.__root, uid)
+
+    def _slot_ptr_del_tmp(self, uid: int):
+        """Del TmpPtr"""
+        del self._tmp_ptr[uid]
